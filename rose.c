439e5b41cc4bb5998550bd6340254e153f9bc8a5
asherlie <lieber.asher@gmail.com>
2018-08-07 00:29:17 -0400

    removed unused code

--- rose.c
+++ rose.c
@@ -4,18 +4,17 @@
 
 #include <vmem_access.h>
 
-bool strtoi(const char* str, unsigned int* ui, int* i){
+bool strtoi(const char* str, int* i){
       char* res;
       unsigned int r = (unsigned int)strtol(str, &res, 10);
       if(*res)return false;
       if(i)*i = (int)r;
-      if(ui)*ui = r;
       return true;
 }
 
 int main(int argc, char** argv){
       pid_t pid;
-      if(argc < 3 || !strtoi(argv[1], NULL, &pid)){
+      if(argc < 3 || !strtoi(argv[1], &pid)){
             puts("enter: pid regex");
             return 1;
       }
@@ -23,13 +22,10 @@ int main(int argc, char** argv){
       regcomp(&reg, argv[2], 0);
       struct mem_map m;
       mem_map_init(&m, pid, true);
-      set_mode_mem_map(&m, false);
       populate_mem_map(&m, BOTH, true, false, 4);
-      int rgn;
-      for(unsigned int i = 0; i < m.size; ++i){
+      for(unsigned int i = 0; i < m.size; ++i)
             if(!regexec(&reg, m.s_mmap[i].value, 0, NULL, 0))
-                  printf("(%s @ %p): \"%s\"\n", which_rgn(m.mapped_rgn, m.s_mmap[i].addr, &rgn), m.s_mmap[i].addr, m.s_mmap[i].value);
-      }
+                  printf("(%5s @ %p): \"%s\"\n", which_rgn(m.mapped_rgn, m.s_mmap[i].addr, NULL), m.s_mmap[i].addr, m.s_mmap[i].value);
       free_mem_rgn(&m.mapped_rgn);
       free_mem_map(&m);
 }
